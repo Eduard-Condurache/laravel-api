a4cfe3b70308d14f890b6e0fa1f5bae9956554ef
Eduard Condurache <eduardcondurache96@gmail.com>
2024-11-17 17:16:34 +0100

    Gestito errori in chiamata api

--- app/Http/Controllers/API/ContactController.php
+++ app/Http/Controllers/API/ContactController.php
@@ -5,9 +5,29 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Models
+use App\Models\Contact;
+use PhpParser\Node\Stmt\TryCatch;
+
 class ContactController extends Controller
 {
     public function store(Request $request) {
-        return response()->json($request->all());
+        
+        try {
+            $data = $request->all();
+
+            $contact = Contact::create($data);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Ok'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid data',
+                'error' => $th->getMessage()
+            ], 400);
+        }
     }
 }
